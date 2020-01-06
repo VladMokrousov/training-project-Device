c1b23506030aca76f4726fa509a4e129b39691d3
Vlad-Mokrousov <mokrousov01@ya.ru>
2020-01-07 00:14:50 +0800

    закончил всю раюлту с js на главной

--- js/script.js
+++ js/script.js
@@ -38,6 +38,79 @@ let timerId = setInterval(() => plusSlides(), 3000);
          }
      }
  });
+
+ //Tabs
+
+ let tabIndex = 1,
+ tabs = document.querySelectorAll('.info-tabcontent'),
+ tabsToggleWrap = document.querySelector('.info-header'),
+ tabsToggle = document.querySelectorAll('.info-header-tab');
+
+showTabs(tabIndex);
+
+ function showTabs(n){
+
+     tabs.forEach((item) => item.style.display = 'none');
+     tabsToggle.forEach((item) => item.classList.remove('active'));
+     tabs[tabIndex - 1].style.display = 'block';
+     tabsToggle[tabIndex - 1].classList.add('active');
+ }
+
+
+ function currentTab(n){
+     showTabs(tabIndex = n);
+ }
+
+
+ tabsToggleWrap.addEventListener('click', function(event){
+     for (let i = 0; i < tabsToggle.length + 1; i++){
+         if(event.target.classList.contains('info-header-tab') && event.target == tabsToggle[i-1]){
+             currentTab(i);
+           
+         }
+     }
+ });
+ //Modal
+
+ let contactsBtn = document.querySelector('.contacts-btn'),
+        modalWriteUs = document.querySelector('.modal-write-us'),
+        overlay = document.querySelector('.overlay'),
+        closeForm = document.querySelector('.close-form'),
+        closeMap = document.querySelector('.close-map'),
+        inputFocus =  document.getElementById('user-name'),
+        imgMap = document.querySelector('.map'),
+        modalMap = document.querySelector('.modal-map');
+
+    contactsBtn.addEventListener('click', function(){
+        
+        overlay.style.display = 'block';
+        modalWriteUs.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+        inputFocus.focus();
+    });
+    closeForm.addEventListener('click', function(){
+        overlay.style.display = 'none';
+        modalWriteUs.style.display = 'none';
+      
+        document.body.style.overflow = '';
+    });
+    imgMap.addEventListener('click', function(){
+        
+        overlay.style.display = 'block';
+        modalMap.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+        
+    });
+   
+   
+    closeMap.addEventListener('click', function(){
+        overlay.style.display = 'none';
+       
+        modalMap.style.display = 'none';
+        document.body.style.overflow = '';
+    });
+   
+    
 });
 
 
